0393b80fd4c57ac677f6ae28a19bf8753eb50dcf
vernal-inertia <tanveer2407@gmail.com>
2020-12-08 13:20:35 +0530

    cython and args

---
+++ argument_parser.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+if __name__ == '__main__':
+    my_parser = ArgumentParser(prog="argument_parser.py",
+                               description="python argument_parser.py A 1.1",
+                               epilog="Enjoy the program! :)\n",
+                               formatter_class=RawTextHelpFormatter)
+    my_parser.add_argument('_string',
+                           type=str,
+                           metavar='_string',                    
+                           help="string variable")
+    my_parser.add_argument('_float',
+                           type=float,
+                           metavar='_float',                           
+                           help="float variable")
+    args = my_parser.parse_args()
+    print(args._string)
+    print(args._float)
---
+++ cython_setup.py
@@ -0,0 +1,8 @@
+import distutils.core
+import Cython.Build
+distutils.core.setup(ext_modules = Cython.Build.cythonize("xyz.pyx"))
+
+"""
+Then run:
+    python cython_setup.py build
+"""
