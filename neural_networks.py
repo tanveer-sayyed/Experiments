e7b38c50bd3fc441ea84a09fbc7f69ac77a4c6fd
vernal-inertia <tanveer2407@gmail.com>
2020-12-09 10:36:43 +0530

    batch_size correction

--- neural_networks.py
+++ neural_networks.py
@@ -4,8 +4,8 @@ import keras
 """
 input_shape = (batch_size, height, width, depth)
 model = keras.Sequential()
-for i in range(3): # architecture with 3 convolution layers
-    model.add(keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation="relu",input_shape=input_shape))
+for i in range(2): # architecture with 3 convolution layers
+    model.add(keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation="relu",input_shape=input_shape[1:]))
     model.add(keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), padding='same'))
     model.add(keras.layers.BatchNormalization())
 # Flatten the output and slowly move towards output layer
@@ -19,11 +19,18 @@ optimizer = keras.optimizers.Adam(learning_rate=0.0001)
 # compile
 model.compile(optimizer=optimizer, loss="categorical_crossentropy",metrics=["accuracy"])
 # fit
-model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=32, epochs=30)
+model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=batch_size, epochs=30)
 # output will be of shape = input_shape
 test_error, test_accuracy = model.evaluate(X_test, y_test, verbose=1)
 
 """
     LSTM
 """
-input_shape = (batch_size, time_steps, units)
+input_shape = (batch_size, time_steps, features)
+model = keras.layers.Sequential()
+model.add(keras.layers.LSTM(50, activation='relu', return_sequences=True, input_shape=input_shape[1:]))
+model.add(keras.layers.LSTM(50, activation='relu'))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=batch_size, epochs=30)
+test_error, test_accuracy = model.evaluate(X_test, y_test, verbose=1)
