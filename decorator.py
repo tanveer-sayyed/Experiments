336df9bc3494daf7838c35252951f072d534b7ec
vernal-inertia <tanveer2407@gmail.com>
2021-02-24 10:53:41 +0530

    added scheduler as decorator

--- decorator.py
+++ decorator.py
@@ -2,6 +2,50 @@
 A decorator is a callable that returns a callable.
 This is similar to packing a gift. The decorator acts as a wrapper.
 """
+from functools import wraps
+def scheduler(tz, hour, minute):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs): 
+            """
+            Parameters
+            ----------
+            function : callable function
+                name of the function which needs to be scheduled
+            tz : str
+                timezone
+            hour : int
+                hour of the time to be scheduled
+            minute : int
+                minute of the time to be scheduled
+            Returns
+            -------
+            None.
+            """
+            while True:
+                hour_ = int(datetime.now(timezone(tz)).time().hour)
+                minute_ = int(datetime.now(timezone(tz)).time().minute)
+                print(f"{hour_}:{minute_} -- {function.__qualname__}")
+                if (hour_ == hour) & (minute_ == minute):
+                    for tries in range(10):
+                        print("Executing -- {function.__qualname__}")
+                        try:
+                            function()
+                            sleep(60)
+                            break
+                        except Exception as exc:
+                            print("Error: Please check logs.")
+                            log_this_error(exc)
+                            continue
+                else:
+                    sleep(58)
+        return wrapper
+    return decorator
+
+@scheduler('Asia/Kolkata', 13, 45)
+def call_me_at(): 
+    print("Hi, You called me at a scheduled time !")
+
 def smart_divide(func):
     def inner(a, b):
         print("I am going to divide", a, "and", b)
@@ -22,3 +66,4 @@ if __name__ == "__main__":
     smart_divide(divide_1)(5, 0)
     divide_2(5, 0)
     divide_2(6, 3)
+    call_me_at()
