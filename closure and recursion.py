d9b5b0ac1081a0e459c061be5be6de08f63e909b
vernal-inertia <tanveer2407@gmail.com>
2021-05-20 12:12:37 +0530

    closure added

--- closure and recursion.py
+++ closure and recursion.py
@@ -11,3 +11,22 @@ def recurring_dot_finder(s):
                 key = key.replace(".","_")
                 s[key] = temp
             recurring_dot_finder(s[key])
+            
+def fib():
+    """
+    Closure works 1000 times faster than recursion
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+    x1 = 0
+    x2 = 1
+    def get_next_number():
+        nonlocal x1, x2
+        x3 = x1 + x2
+        x1, x2 = x2, x3
+        return x3
+    return get_next_number
