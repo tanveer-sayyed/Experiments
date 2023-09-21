1f6a143934c791bc8d3827776308a32e1686366c
tanveer-sayyed <tanveer2407@gmail.com>
2023-09-21 11:16:10 +0530

    trigram updated

--- basics/ak/trigram.py
+++ basics/ak/trigram.py
@@ -4,7 +4,7 @@
 """
 
 from json import loads
-from torch import Generator, multinomial, tensor, zeros
+from torch import float32, Generator, multinomial, tensor, zeros
 
 END = "~" # end token
 START = "_" # start token
@@ -13,11 +13,9 @@ G = Generator().manual_seed(2)
 prob = {
     "ch1" : {},
     "ch2|ch1" : {},
-    "ch3|ch1,ch2":{}
+    "ch3|ch1,ch2":{},
     }
 counter = {}
-final_prob = prob.copy()
-
 words = loads(open("names.txt","r").read())
 words = words['payload']['blob']['rawLines']
 words = [START + word + END for word in words]
@@ -27,65 +25,59 @@ char2int = {c:i for i,c in enumerate(unique)}
 for word in words:
     for trigram in zip(word, word[1:], word[2:]):
         counter[trigram] = counter.get(trigram, 0) + 1
-matrix = zeros(size=(len(unique),len(unique),len(unique)))
+matrix = zeros(size=(len(unique),len(unique),len(unique)), dtype=float32)
 for ch1, ch2, ch3 in counter.keys():     
         matrix[char2int[ch1], char2int[ch2], char2int[ch3]] = \
             counter[(ch1, ch2, ch3)]
 
-## prob(ch1)
+## prob(ch1) :: O(n)
 for ch1 in unique: prob["ch1"][ch1] = matrix[char2int[ch1],:,:].sum().item()
 prob["ch1"] = dict(sorted(prob["ch1"].items()))
 temp = sum(prob["ch1"].values())
 for key in prob["ch1"].keys(): prob["ch1"][key] /= temp
-final_prob["ch1"] = tensor(list(prob["ch1"].values()))
+prob["ch1"] = tensor(list(prob["ch1"].values()))
 
-# prob(ch2|ch1)
-for ch2 in unique:
-    for ch1 in unique:
-        prob["ch2|ch1"][f"{ch2}|{ch1}"] = \
-            matrix[char2int[ch1], char2int[ch2], :].sum().item()
+# prob(ch2|ch1) :: O(n**2)
 for ch1 in unique:
     values = []
-    for ch2 in unique: values.append(prob["ch2|ch1"][f"{ch2}|{ch1}"])
-    final_prob["ch2|ch1"][f"*|{ch1}"] = tensor(values)
+    for ch2 in unique: values.append(
+            matrix[char2int[ch1], char2int[ch2], :].sum().item()
+            )
+    prob["ch2|ch1"][f"*|{ch1}"] = tensor(values)
     if sum(values) != 0.0: 
-        final_prob["ch2|ch1"][f"*|{ch1}"] /= tensor(values).sum()
+        prob["ch2|ch1"][f"*|{ch1}"] /= tensor(values).sum()
 
-# prob(ch3|ch1,ch2)
-for ch3 in unique:
-    for ch2 in unique:
-        for ch1 in unique:
-            prob["ch3|ch1,ch2"][f"{ch3}|{ch1},{ch2}"] = \
-                matrix[char2int[ch1], char2int[ch2], char2int[ch3]].item()
-all_keys = list(prob["ch3|ch1,ch2"].keys())
+# prob(ch3|ch1,ch2) :: O(n**3)
 for ch1 in unique:
     for ch2 in unique:
         values = []
-        keys = [s for s in all_keys if s.endswith(f"{ch1},{ch2}")]
-        for key in keys: values.append(prob["ch3|ch1,ch2"][key])
-        final_prob["ch3|ch1,ch2"][f"*|{ch1},{ch2}"] = tensor(values)
+        for ch3 in unique:
+            values.append(
+                    matrix[char2int[ch1], char2int[ch2], char2int[ch3]].item()
+                    )
+        prob["ch3|ch1,ch2"][f"*|{ch1},{ch2}"] = tensor(values)
         if sum(values) != 0.0:
-            final_prob["ch3|ch1,ch2"][f"*|{ch1},{ch2}"] /= tensor(values).sum()
+            prob["ch3|ch1,ch2"][f"*|{ch1},{ch2}"] /= tensor(values).sum()
 
 # prediction
 for _ in range(5):
     out = []
     counter = 0
     while True:
-        if counter == 0:               # code-block for 1st letter
+        if counter == 0:               # 1st letter
             idx = char2int[START]      # always begin with the start token
             out.append(int2char[idx])
-        elif counter == 1:             # code-block for 2nd letter
+        elif counter == 1:             # 2nd letter
             idx = multinomial(
-                input=final_prob["ch2|ch1"][f"*|{out[-1]}"],
+                input=prob["ch2|ch1"][f"*|{out[-1]}"],
                 generator=G,
                 num_samples=1,
                 replacement=True,
             ).item()
             out.append(int2char[idx])
-        else:                          # after the first two letters,
+        else:                          # for subsequent letters,
             idx = multinomial(         # keep using this code-block ...
-                input=final_prob["ch3|ch1,ch2"][f"*|{out[-2]},{out[-1]}"],
+                input=prob["ch3|ch1,ch2"][f"*|{out[-2]},{out[-1]}"],
                 generator=G,
                 num_samples=1,
                 replacement=True,
@@ -94,12 +86,11 @@ for _ in range(5):
         if idx == char2int[END]: break # ... until the end token is encountered
         counter += 1
     print("".join(out))
-
-"""
-    Output: 
-        _tavi~
-        _kallee~
-        _lour~
-        _cohnn~
-        _magrah~
-"""
+    """
+        Output: 
+            _tavi~
+            _kallee~
+            _lour~
+            _cohnn~
+            _magrah~
+    """
