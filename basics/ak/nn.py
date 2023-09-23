6268148ccd8165885a706c4d105773124a682949
tanveer-sayyed <tanveer2407@gmail.com>
2023-09-23 09:44:42 +0530

    forward and backward passes

--- basics/ak/nn.py
+++ basics/ak/nn.py
@@ -1,3 +1,12 @@
+"""
+    [reference]: https://github.com/karpathy/nn-zero-to-hero/blob/master/lectures/micrograd/micrograd_lecture_second_half_roughly.ipynb
+    
+a neural network from scratch, with just the forward pass
+for backward pass see:
+    chainRule1.ipynb
+    chainRule2.ipynb
+"""
+
 from dataclasses import dataclass, field
 from random import uniform
 from typing import List
@@ -13,10 +22,6 @@ class Neuron:
         self.weight = [uniform(-1,1) for _ in range(self.nn_in)]
 
     def __call__(self, x:float) -> float:
-        print(self.weight)
-        print(x)
-        for (wi, xi) in zip(self.weight, x):
-            print(wi, xi)
         return sum(
             [xi*wi for (xi, wi) in zip(x, self.weight)],
             self.bias
@@ -68,40 +73,3 @@ class MLP:
 
 
 mlp = MLP(3, (4,2,1))
-mlp.layers
-mlp.layers[0].neurons
-mlp.layers[1].neurons
-mlp.layers[2].neurons
-mlp.layers[0].neurons[0].parameters
-mlp.layers[0].neurons[1].parameters
-mlp.layers[0].neurons[2].parameters
-mlp.layers[0].parameters
-mlp.parameters
-xs = [[1,2,3],[4,5,6]]
-[mlp(x) for x in xs]
-
-xs = [
-  [2.0, 3.0, -1.0],
-  [3.0, -1.0, 0.5],
-  [0.5, 1.0, 1.0],
-  [1.0, 1.0, -1.0],
-]
-mlp(xs)
-ys = [1.0, -1.0, -1.0, 1.0] # desired targets
-
-for k in range(20):
-  
-  # forward pass
-  ypred = [n(x) for x in xs]
-  loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
-
-  # backward pass
-  for p in n.parameters:
-    p.grad = 0.0
-  loss.backward()
-
-  # update
-  for p in n.parameters:
-    p.data += -0.1 * p.grad
-
-  print(k, loss.data)
