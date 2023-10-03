671c3ff5d010cf7675909ad126282417a961ea11
tanveer-sayyed <tanveer2407@gmail.com>
2023-10-03 13:27:36 +0530

    loss calculated

--- basics/ak/backwardPass/buildDataset_5.py
+++ basics/ak/backwardPass/buildDataset_5.py
@@ -7,26 +7,21 @@ Keep in mind:
         - grads might accumulate
         - grads might not accumulate
     5. variables, only meant for numerical stability, should have grad = 0.0
-    6. broadcast in forward pass means a sum in backward pass
+    6. braoadcast in forward pass means a sum in backward pass
     7. sum in forward pass means a broadcast in backward pass
 """
 from string import ascii_lowercase
-from torch import Generator, randn, tensor
+from torch import Generator, tensor
 
+# reproducible results
 G = Generator().manual_seed(103)
 
 data = [c for c in ascii_lowercase]
 vocab_size = len(data) # unique
 char2int = {c:i for (i,c) in enumerate(data)}
 int2char = {i:c for (i,c) in enumerate(data)}
-embedding_dim = 1
-embeddings = randn(
-    size=(vocab_size, embedding_dim),
-    generator=G
-    )
 
-# a->b | b->c | ... | z->a :: no tts possible here
+# out dataset is as follow:
+#       a->b | b->c | ... | z->a :: no tts possible here
 X = tensor([char2int[char] for char in data])
-y = tensor(
-        [char2int[int2char[(i+1)%vocab_size]] for i in range(vocab_size)]
-           )
+y = tensor([char2int[int2char[(k+1)%vocab_size]] for k in int2char.keys()])
--- basics/ak/backwardPass/buildNN_6.py
+++ basics/ak/backwardPass/buildNN_6.py
@@ -1,16 +1,50 @@
-from torch import randint
+from torch import arange, randint, randn
+from torch.nn.functional import cross_entropy
 
-from buildDataset_5 import G, X, y, embeddings
+from buildDataset_5 import (
+    G,
+    vocab_size,
+    X,
+    y,
+    )
 
+ACROSS_COLUMNS = 0 # helper variable
+ACROSS_ROWS = 1    # helper variable
 BATCH_SIZE = n = 2
+# our characters would be scattered within this space of these number of dimensions
+embedding_dim = 2 
+neurons_in_hidden_layer = 26
+# creating the mapping to scatter the characters onto this space
+embeddings = randn(
+    size=(vocab_size, embedding_dim),
+    generator=G
+    )
 
-X_train = embeddings[X]
-y_train = embeddings[y]
+X_train = embeddings[X] # scattered the training data
+idxs = randint(0, X_train.shape[0], (BATCH_SIZE,), generator=G) # random batch
+X_train_batch, y_train_batch = X_train[idxs], y[idxs]
+# a manual linear layer with its own weights and biases
+weights = randn(
+    size=(embedding_dim, neurons_in_hidden_layer), generator=G
+    ) * (5/3) / (embedding_dim**0.5) # kiaming_normal with golden ration
+bias = randn(size=(neurons_in_hidden_layer,), generator=G) * 0.1
 
-parameters = [embeddings]
-for p in parameters: p.requires_grad = True
-X_train.backward()
+# initialise parameters
+parameters = [embeddings, weights, bias]
+for p in parameters: p.requires_grad = True # else None
 
-# create a random batch
-idxs = randint(0, X_train.shape[0], (n,), generator=G)
-X_train_batch, y_train_batch = X_train[idxs], y_train[idxs]
+# running data through the nn
+logits = X_train_batch @ weights + bias
+# manually implement cross entropy
+logits_maxes = logits.max(ACROSS_ROWS, keepdims=True) # get max to normalise
+# postive numbers can overflow exponential
+# also because final probabilities are invariant to offsets
+logits_normalised = logits - logits_maxes.values
+# expanding log signals to increase the distance between them
+exponentiated = logits_normalised.exp()
+# normalise, to get probabilities
+probabilities = exponentiated / exponentiated.sum(ACROSS_ROWS, keepdims=True)
+# negativeNormalisedLogLikelihood
+loss = -probabilities[arange(n), y_train_batch].log().mean()
+print(cross_entropy(logits, y_train_batch), loss)
+# based on this loss the rearrangement of the scattered points will take place
