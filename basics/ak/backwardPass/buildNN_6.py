b8e4027b134e277df5ed394ed65fccda81d4ab2e
tanveer-sayyed <tanveer2407@gmail.com>
2023-10-07 20:47:11 +0530

    backward_pass

--- basics/ak/backwardPass/buildNN_6.py
+++ basics/ak/backwardPass/buildNN_6.py
@@ -1,5 +1,5 @@
 from torch import (
-    arange, float32, ones_like, randint, randn, tensor, zeros_like
+    allclose, arange, float32, ones_like, randint, randn, tensor, zeros_like
     )
 from torch.nn.functional import cross_entropy
 
@@ -14,14 +14,15 @@ BATCH_SIZE = n = 2
 # within this space; this space has 2 dimensions; (2 is actually too small)
 embedding_dim = 2
 neurons_in_hidden_layer = 26
-# creating the mapping to scatter randomly the characters onto this space
+# creating the mapping to scatter the characters randomly onto this space
 embeddings = randn(
     size=(vocab_size, embedding_dim),
     generator=G
     )
-
-X_train = embeddings[X] # get the randomly scattered embeddings of the training data
-idxs = randint(0, X_train.shape[0], (BATCH_SIZE,), generator=G) # sample a random batch
+# get the randomly scattered embeddings of the training data
+X_train = embeddings[X]
+# get a random batch
+idxs = randint(0, X_train.shape[0], (BATCH_SIZE,), generator=G)
 X_train_batch, y_train_batch = X_train[idxs], y[idxs]
 
 # a manual linear layer with its own weights and biases
@@ -34,66 +35,100 @@ bias = randn(size=(neurons_in_hidden_layer,), generator=G) * 0.1
 parameters = [embeddings, weights, bias]
 for p in parameters: p.requires_grad = True # else None
 
-# running data through the nn
+# Manual backward pass
 logits = X_train_batch @ weights + bias
-### manually implementing cross entropy
-# postive numbers can overflow exponential, also because final probabilities are invariant to offsets
-logits_normalised = logits - logits.max(ALONG_ROWS, keepdims=True).values
+# 1. postive numbers can overflow exponential
+# 2. final probabilities are invariant to offsets
+# 3. which means the derivatives of these offsets must be (close to) zero
+logits_max_along_rows = logits.max(ALONG_ROWS, keepdims=True).values
+logits_normalised = logits - logits_max_along_rows ## <--
 # expanding log signals to increase the distance between them
 exponentiated = logits_normalised.exp()
 exponentiated_sum_along_rows = exponentiated.sum(ALONG_ROWS, keepdims=True)
 # normalise, to get probabilities
-all_probabilities = exponentiated / exponentiated_sum_along_rows # <---
-req_probabilities = all_probabilities[arange(n), y_train_batch]
-log_probabilities = req_probabilities.log()
-mean_log_probabilities = log_probabilities.mean()
-# negativeNormalisedLogLikelihood
-negative_mean_log_probabilities = -mean_log_probabilities
-print(cross_entropy(logits, y_train_batch), negative_mean_log_probabilities)
+normalised_exponents = exponentiated / exponentiated_sum_along_rows
+# select only the required ones (current probabilities in our batch)
+req_normalised_exponents = normalised_exponents[arange(n), y_train_batch]
+# after getting probabilities lets return to the log world
+log_probabilities = req_normalised_exponents.log()
+# likelihood is just a single number denoting probability, here it's mean
+mean_log_likelihood = log_probabilities.mean()
+negative_mean_log_likelihood = -mean_log_likelihood
+print(cross_entropy(logits, y_train_batch), negative_mean_log_likelihood)
 # based on this loss the rearrangement of the scattered points will take place
 
-# PyTorch backward pass
-for p in parameters: p.grad = None
-for variables in [
-        logits, logits_normalised, exponentiated, exponentiated_sum_along_rows,
-        all_probabilities, req_probabilities, log_probabilities,
-        mean_log_probabilities, negative_mean_log_probabilities
-        ]: variables.retain_grad()
-negative_mean_log_probabilities.backward() # loss.backward()
+# these are the variables in our DAG
+variables = [
+        'logits',
+        'logits_max_along_rows',
+        'logits_normalised',
+        'exponentiated',
+        'exponentiated_sum_along_rows',
+        'normalised_exponents',
+        'req_normalised_exponents',
+        'log_probabilities',
+        'mean_log_likelihood',
+        'negative_mean_log_likelihood' # <-- loss
+        ]
 
-print("calculating drivatives using chain rule::")
-mean_log_probabilities_global_grad = tensor([-1.0])
-print(f"{mean_log_probabilities_global_grad.item()=}")
+### PyTorch backward pass
+for p in parameters: p.grad = None
+for v in variables: eval(v).retain_grad()
+negative_mean_log_likelihood.backward() # loss.backward()
+
+
+### Check if manual grads == pytorch autograds
+negative_mean_log_likelihood_global_grad = tensor(1.0) # see chainRule_2.ipynb
+mean_log_probabilities_global_grad = tensor([-1.0]) * \
+                                    negative_mean_log_likelihood_global_grad
 log_probabilities_local_grad = ones_like(log_probabilities, dtype=float32) * 0.5
-print(f"\t{log_probabilities_local_grad=}", )
-log_probabilities_global_grad = log_probabilities_local_grad * mean_log_probabilities_global_grad
-print(f"{log_probabilities_global_grad=}")
-req_probabilities_local_grad = 1/req_probabilities
-print(f"\t{req_probabilities_local_grad=}")
-req_probabilities_global_grad = req_probabilities_local_grad * log_probabilities_global_grad
-print(f"\t{req_probabilities_global_grad=}")
-all_probabilities_global_grad = zeros_like(all_probabilities, dtype=float32)
-all_probabilities_global_grad[arange(n), y_train_batch] = req_probabilities_global_grad
-print(f"{all_probabilities_global_grad=}")
-# global derivative must be directly multiplied during sum/broadcast
+log_probabilities_global_grad = log_probabilities_local_grad * \
+                                    mean_log_probabilities_global_grad
+req_normalised_exponents_local_grad = 1/req_normalised_exponents
+req_normalised_exponents_global_grad = req_normalised_exponents_local_grad * \
+                                    log_probabilities_global_grad
+normalised_exponents_global_grad = zeros_like(
+    normalised_exponents, dtype=float32
+    )
+normalised_exponents_global_grad[
+    arange(n), y_train_batch
+    ] = req_normalised_exponents_global_grad
+# global derivative must be directly multiplied before sum/broadcast
 exponentiated_sum_along_rows_global_grad = (
-     -exponentiated/exponentiated_sum_along_rows**2 * all_probabilities_global_grad
+     -exponentiated/exponentiated_sum_along_rows**2 * \
+         normalised_exponents_global_grad
      ).sum(ALONG_ROWS, keepdim=True)
-print(f"{exponentiated_sum_along_rows_global_grad=}")
-exponentiated_local_grad_1 = exponentiated_sum_along_rows **(-1)
-exponentiated_global_grad_1 = exponentiated_local_grad_1 * all_probabilities_global_grad
+exponentiated_local_grad_1 = exponentiated_sum_along_rows**(-1)
+# and from chainRule_2.ipynb we get
+exponentiated_global_grad_1 = exponentiated_local_grad_1 * \
+                                  normalised_exponents_global_grad
 # in a sum operation the derivatives flow equally to all its elements of the tensor
-exponentiated_global_grad_2 = ones_like(exponentiated, dtype=float32) * exponentiated_sum_along_rows_global_grad
-exponentiated_global_grad = exponentiated_global_grad_1 + exponentiated_global_grad_2
+exponentiated_global_grad_2 = ones_like(exponentiated, dtype=float32) * \
+                                  exponentiated_sum_along_rows_global_grad
+exponentiated_global_grad = exponentiated_global_grad_1 + \
+                                exponentiated_global_grad_2
 logits_normalised_local = logits_normalised.exp()
-logits_normalised_global = logits_normalised_local * exponentiated_global_grad
-# print(f"{=}")
-# print(f"{=}")
-# print(f"{=}")
-mean_log_probabilities.grad
-log_probabilities.grad
-req_probabilities.grad
-all_probabilities.grad
-exponentiated_sum_along_rows.grad
-exponentiated.grad
-logits_normalised.grad
+logits_normalised_global_grad = logits_normalised_local * \
+                                    exponentiated_global_grad
+# in a sum operation the derivatives flow equally to all its elements of the tensor
+logits_global_grad_1 = logits_normalised_global_grad.clone()
+# broadcasting in forward --> sum in backward
+# note that it this is only a offset added for numerical stability, hence (all close) to zero
+logits_max_along_rows_global_grad = -logits_normalised_global_grad.sum(
+                                        ALONG_ROWS, keepdims=True
+                                        )
+logits_global_grad_2 = zeros_like(
+    normalised_exponents, dtype=float32
+    )
+
+for v in variables[2:]: print(allclose(eval(v).grad, eval(v+"_global_grad")))
+
+"""
+logits = X_train_batch @ weights + bias
+
+a00 a01     w00 w01 w02     a00*w00+a01*w10  a00*w01+a01*w11  a00*w02+a01*w12
+a10 a11  @  w10 w11 w12  =  a10*w00+a11*w10  a10*w01+a11*w11  a10*w01+a11*w11
+
+
+
+"""
--- basics/ak/backwardPass/chainRuleUtils.py
+++ basics/ak/backwardPass/chainRuleUtils.py
@@ -4,25 +4,25 @@ def grad_of_a_during_addition(a, b):
     z = a + b; z.label = "z"
     a.data = a.data + a_very_small_value
     Z = a + b; Z.label = "z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b_during_addition(a, b):
     z = a + b; z.label = "z"
     b.data = b.data + a_very_small_value
     Z = a + b; Z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_a_during_multiplication(a, b):
     z = a * b; z.label = "z"
     a.data = a.data + a_very_small_value
     Z = a * b; Z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b_during_multiplication(a, b):
     z = a * b; z.label = "z"
     b.data = b.data + a_very_small_value
     Z = a * b; Z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 
 def grad_of_a1(a, b, d):
@@ -32,7 +32,7 @@ def grad_of_a1(a, b, d):
     a.data = a.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b1(a, b, d):
     c = a + b; c.label = "c"
@@ -41,16 +41,16 @@ def grad_of_b1(a, b, d):
     b.data = b.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_c1(a, b, d):
     c = a + b; c.label = "c"
     z = c * d; z.label = "z"
-    
+
     c = a + b; c.label = "c"
     c.data = c.data + a_very_small_value
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_d1(a, b, d):
     c = a + b; c.label = "c"
@@ -59,7 +59,7 @@ def grad_of_d1(a, b, d):
     c = a + b; c.label = "c"
     d.data = d.data + a_very_small_value
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_a2(a, b, d):
     c = a * b; c.label = "c"
@@ -68,7 +68,7 @@ def grad_of_a2(a, b, d):
     a.data = a.data + a_very_small_value
     c = a * b; c.label = "c"
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b2(a, b, d):
     c = a * b; c.label = "c"
@@ -77,16 +77,16 @@ def grad_of_b2(a, b, d):
     b.data = b.data + a_very_small_value
     c = a * b; c.label = "c"
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_c2(a, b, d):
     c = a * b; c.label = "c"
     z = c + d; z.label = "z"
-    
+
     c = a * b; c.label = "c"
     c.data = c.data + a_very_small_value
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_d2(a, b, d):
     c = a * b; c.label = "c"
@@ -95,7 +95,7 @@ def grad_of_d2(a, b, d):
     c = a * b; c.label = "c"
     d.data = d.data + a_very_small_value
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_a3(a, b, d):
     c = a * b; c.label = "c"
@@ -104,7 +104,7 @@ def grad_of_a3(a, b, d):
     a.data = a.data + a_very_small_value
     c = a * b; c.label = "c"
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b3(a, b, d):
     c = a * b; c.label = "c"
@@ -113,16 +113,16 @@ def grad_of_b3(a, b, d):
     b.data = b.data + a_very_small_value
     c = a * b; c.label = "c"
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_c3(a, b, d):
     c = a * b; c.label = "c"
     z = c * d; z.label = "z"
-    
+
     c = a * b; c.label = "c"
     c.data = c.data + a_very_small_value
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_d3(a, b, d):
     c = a * b; c.label = "c"
@@ -131,7 +131,7 @@ def grad_of_d3(a, b, d):
     c = a * b; c.label = "c"
     d.data = d.data + a_very_small_value
     Z = c * d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_a4(a, b, d):
     c = a + b; c.label = "c"
@@ -140,7 +140,7 @@ def grad_of_a4(a, b, d):
     a.data = a.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b4(a, b, d):
     c = a + b; c.label = "c"
@@ -149,16 +149,16 @@ def grad_of_b4(a, b, d):
     b.data = b.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_c4(a, b, d):
     c = a + b; c.label = "c"
     z = c + d; z.label = "z"
-    
+
     c = a + b; c.label = "c"
     c.data = c.data + a_very_small_value
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_d4(a, b, d):
     c = a + b; c.label = "c"
@@ -167,7 +167,7 @@ def grad_of_d4(a, b, d):
     c = a + b; c.label = "c"
     d.data = d.data + a_very_small_value
     Z = c + d; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_a5(a, b):
     c = a + b; c.label = "c"
@@ -176,7 +176,7 @@ def grad_of_a5(a, b):
     a.data = a.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c + b; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b5(a, b):
     c = a + b; c.label = "c"
@@ -185,16 +185,16 @@ def grad_of_b5(a, b):
     b.data = b.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c + b; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_c5(a, b):
     c = a + b; c.label = "c"
     z = c + b; z.label = "z"
-    
+
     c = a + b; c.label = "c"
     c.data = c.data + a_very_small_value
     Z = c + b; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_a6(a, b):
     c = a + b; c.label = "c"
@@ -203,7 +203,7 @@ def grad_of_a6(a, b):
     a.data = a.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c * b; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_b6(a, b):
     c = a + b; c.label = "c"
@@ -212,13 +212,13 @@ def grad_of_b6(a, b):
     b.data = b.data + a_very_small_value
     c = a + b; c.label = "c"
     Z = c * b; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
 
 def grad_of_c6(a, b):
     c = a + b; c.label = "c"
     z = c * b; z.label = "z"
-    
+
     c = a + b; c.label = "c"
     c.data = c.data + a_very_small_value
     Z = c * b; z.label = "Z"
-    return (Z.data - z.data)/a_very_small_value # <-- [f(z+h) - f(z)] / h
+    return (Z.data - z.data)/a_very_small_value # <-- [f(loss+h) - f(loss)] / h
